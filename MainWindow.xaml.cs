348647e517c1298399efcadb55ea83108702cb81
PieceOfEon <danilskripka1996@gmail.com>
2022-12-02 19:40:07 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rozetka
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ru_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ru.Foreground = Brushes.Red;
+        }
+
+        private void ru_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ru.Foreground = Brushes.White;
+        }
+
+        private void UA_MouseEnter(object sender, MouseEventArgs e)
+        {
+            UA.Foreground = Brushes.Red;
+        }
+
+        private void UA_MouseLeave(object sender, MouseEventArgs e)
+
+        { 
+            LinearGradientBrush myBrush = new LinearGradientBrush();
+            myBrush.StartPoint = new Point(0.5,1);
+            myBrush.EndPoint = new Point(0.5,0);
+            myBrush.GradientStops.Add(new GradientStop(Colors.Yellow, 0.7));
+            myBrush.GradientStops.Add(new GradientStop(Colors.Blue, 0.3));
+            UA.Foreground = myBrush;
+
+        }
+    }
+}
