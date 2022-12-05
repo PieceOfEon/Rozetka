a19be8f30c9334b0e103359ff73ba6bf39fa0889
PieceOfEon <danilskripka1996@gmail.com>
2022-12-05 11:46:02 +0200

    end

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,11 +15,11 @@ using System.Windows.Shapes;
 
 namespace Rozetka
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
+   
     public partial class MainWindow : Window
     {
+        private int kol = 1;
         public MainWindow()
         {
             InitializeComponent();
@@ -50,6 +50,114 @@ namespace Rozetka
             myBrush.GradientStops.Add(new GradientStop(Colors.Blue, 0.3));
             UA.Foreground = myBrush;
 
+        }
+
+        private void Menu_ContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            kol++;
+            if (kol == 8)
+            {
+                floor1.Visibility=Visibility.Hidden;
+                //kol = 1;
+            }
+            else
+            {
+                floor1.Visibility = Visibility.Visible;
+            }
+            if (kol == 1)
+            {
+                imgContent.Content = Resources["Image.1"];
+                floor1.Visibility = Visibility.Visible;
+            }
+            else if (kol == 2)
+            {
+                imgContent.Content = Resources["Image.2"];
+                floor2.Visibility = Visibility.Visible;
+            }
+            else if (kol == 3)
+            {
+                imgContent.Content = Resources["Image.3"];
+            }
+            else if (kol == 4)
+            {
+                imgContent.Content = Resources["Image.4"];
+            }
+            else if (kol == 5)
+            {
+                imgContent.Content = Resources["Image.5"];
+            }
+            else if (kol == 6)
+            {
+                imgContent.Content = Resources["Image.6"];
+            }
+            else if (kol == 7)
+            {
+                imgContent.Content = Resources["Image.7"];
+            }
+            else if (kol == 8)
+            {
+                imgContent.Content = Resources["Image.8"];
+            }
+            
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            kol--;
+            if (kol == 1)
+            {
+                floor2.Visibility = Visibility.Hidden;
+                //kol = 1;
+            }
+            else
+            {
+                floor1.Visibility = Visibility.Visible;
+            }
+            if (kol == 1)
+            {
+                imgContent.Content = Resources["Image.1"];
+                floor1.Visibility = Visibility.Visible;
+            }
+            else if (kol == 2)
+            {
+                imgContent.Content = Resources["Image.2"];
+                floor2.Visibility = Visibility.Visible;
+            }
+            else if (kol == 3)
+            {
+                imgContent.Content = Resources["Image.3"];
+                floor2.Visibility = Visibility.Visible;
+            }
+            else if (kol == 4)
+            {
+                imgContent.Content = Resources["Image.4"];
+                floor2.Visibility = Visibility.Visible;
+            }
+            else if (kol == 5)
+            {
+                imgContent.Content = Resources["Image.5"];
+                floor2.Visibility = Visibility.Visible;
+            }
+            else if (kol == 6)
+            {
+                imgContent.Content = Resources["Image.6"];
+                floor2.Visibility = Visibility.Visible;
+            }
+            else if (kol == 7)
+            {
+                imgContent.Content = Resources["Image.7"];
+                floor2.Visibility = Visibility.Visible;
+            }
+            else if (kol == 8)
+            {
+                imgContent.Content = Resources["Image.8"];
+                floor2.Visibility = Visibility.Visible;
+            }
         }
     }
 }
